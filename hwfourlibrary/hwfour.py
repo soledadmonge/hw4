7727fac9a000e9d0f82bedda743a2b34eb1551bb
Anastasia Chernavskaia <anaschern@gmail.com>
2024-10-31 19:59:08 +0100

    Add exercises 3 and 4 to hwfour.py

--- hwfourlibrary/hwfour.py
+++ hwfourlibrary/hwfour.py
@@ -15,4 +15,34 @@ def get_day_month_year(dates):
     dates = date_list
     return df
 
+# %%
+import geopy
+from geopy.distance import geodesic
 
+# To calculate the distance of two points we can use "geodesic" or "great_circle". We are going to use the first one:
+def compute_distance(pairs):
+    # We use lambda to calculate the distance between each pair of coord (in this case, coords[0] and coords[1]). 
+    # "map" applies this lambda to each element of "pairs". 
+    # The result is an iterable with the distances, which we then convert to a list.
+    return list(map(lambda coords: geodesic(coords[0], coords[1]).kilometers, pairs))
+
+pairs = [((41.23, 23.5), (41.5, 23.4)), ((52.38, 20.1), (52.3, 17.8))]
+dist_km = compute_distance(pairs)
+dist_km
+
+# %% [markdown]
+# ### Exercise 4:
+# Consider a list that each element can be an integer or a list that contains integers or more lists with integers example: [[2], 4, 5, [1, [2], [3, 5, [7,8]], 10], 1]. Create a recursive function called "sum_general_int_list" that takes as input this type of list and returns the sum of all the integers within the lists for instance for list_1=[[2], 3, [[1,2],5]] the result should be 13.
+
+# %%
+def sum_general_int_list(list_ex):
+    # We use lambda and isistance to check if an element "x" is a list or an integer:
+     # a) If "x" is a list, the function makes a recursive call to sum_general_int_list(x) to decompose that list and sum its elements.
+     # b) If "x" is an integer, lambda returns the value of "x" and adds it.
+    # "map" applies this lambda to each element of "list". 
+    # The result is the sum of all the elements.
+    return sum(map(lambda x: sum_general_int_list(x) if isinstance(x, list) else x, list_ex))
+
+list_ex = [[2], 3, [[1, 2], 5]]
+result = sum_general_int_list(list_ex)
+result
