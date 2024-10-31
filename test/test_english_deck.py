c749c19c71d5bb81bd67a1b6770be827b977e644
Anastasia Chernavskaia <anaschern@gmail.com>
2024-10-31 21:04:37 +0100

    Add english card test

---
+++ test/test_english_deck.py
@@ -0,0 +1,27 @@
+# %%
+import sys
+import os
+import pytest
+
+sys.path.append(os.path.abspath("../"))  
+from hw5_lib.hw5_lib.hw5 import Card, Deck
+
+# %%
+def test__card():
+    card = Card('Hearts', 'A')
+    
+    assert card.suit == 'Hearts', "Suit must be 'Hearts'"
+    assert card.value == 'A', "The value must be 'A'"
+
+# %%
+def test__deck():
+    deck = Deck()
+    t_suits = {'Hearts', 'Diamonds', 'Clubs', 'Spades'}
+    t_values = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'}
+
+    assert {card.suit for card in deck.cards} == t_suits, "The Deck must have all the suits"
+    assert {card.value for card in deck.cards} == t_values, "The Deck must have all de values"
+    assert len(deck.cards) == 52, "The Deck must have 52 cards"
+
+
+
